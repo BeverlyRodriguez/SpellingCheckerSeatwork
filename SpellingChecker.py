7915988db1c4f9ccbe80322776d76491a0fb0126
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-19 16:49:02 +0800

    Wrong/Deleting def check

--- SpellingChecker.py
+++ SpellingChecker.py
@@ -8,9 +8,5 @@ root.geometry("1000x700")
 root.config(background="black")
 
 
-def check():
-    b = TextBlob(e.get())
-    corrected_text = Label(root, text = str(b.correct()))
-    corrected_text.pack()
     
 root.mainloop()
