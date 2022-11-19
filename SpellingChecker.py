5a785aa48bb95923bbc75677d4372e5a141c2917
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-19 16:51:59 +0800

    Heading Design

--- SpellingChecker.py
+++ SpellingChecker.py
@@ -8,5 +8,6 @@ root.geometry("1000x700")
 root.config(background="black")
 
 
-    
+heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), bg="#white", fg="#gray") 
+
 root.mainloop()
