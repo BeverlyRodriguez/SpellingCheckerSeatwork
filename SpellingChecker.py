c335f2971b7da1cdcae9c32d6fdf13f6818ee27a
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-19 17:06:30 +0800

    Design Colors

--- SpellingChecker.py
+++ SpellingChecker.py
@@ -8,10 +8,10 @@ root.geometry("1000x700")
 root.config(background="black")
 
 
-heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), bg="#CDC0B0", fg="#gray") 
+heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), bg="#CDC0B0", fg="	#8B8378") 
 heading.pack (pady=(50,0))
 
-text = Entry(root, justify="center", width = 15, font=("poppins", 25), bg="blue", border=2)
+text = Entry(root, justify="center", width = 15, font=("poppins", 25), bg="#E3CF57", border=2)
 text.pack(pady=10)
 text.focus()
 root.mainloop()
