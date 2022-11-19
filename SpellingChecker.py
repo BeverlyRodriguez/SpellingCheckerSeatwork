79a72c1b0bc20b803e6be6f19f95ca88d6b1cd58
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-19 16:46:41 +0800

    Add loop/def check

--- SpellingChecker.py
+++ SpellingChecker.py
@@ -1,2 +1,16 @@
 import tkinter
 from tkinter import *
+from textblob import TextBlob
+
+root = Tk()
+root.title("Spelling Checker")
+root.geometry("1000x700")
+root.config(background="black")
+
+
+def check():
+    b = TextBlob(e.get())
+    corrected_text = Label(root, text = str(b.correct()))
+    corrected_text.pack()
+    
+root.mainloop()
