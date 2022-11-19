6b777ec2fb1355b327a415341cfe6a276c9f4e2e
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-19 17:19:40 +0800

    Button

--- SpellingChecker.py
+++ SpellingChecker.py
@@ -4,14 +4,19 @@ from textblob import TextBlob
 
 root = Tk()
 root.title("Spelling Checker")
-root.geometry("1000x700")
-root.config(background="black")
+root.geometry("700x400")
+root.config(background="#E6E6FA")
 
 
-heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), bg="#CDC0B0", fg="	#8B8378") 
+heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), fg="#333333") 
 heading.pack (pady=(50,0))
 
-text = Entry(root, justify="center", width = 15, font=("poppins", 25), bg="#E3CF57", border=2)
+text = Entry(root, justify="center", width = 15, font=("poppins", 25), bg="#FFF0F5", border=2)
 text.pack(pady=10)
 text.focus()
+
+button = Button(root, text="CHECK", width=15, font=("arial", 20, "bold"), fg="#FAEBD7", bg="#FFEFDB")
+button.pack()
+
+
 root.mainloop()
