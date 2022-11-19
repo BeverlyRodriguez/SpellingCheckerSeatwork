9a88cbc4ed78b4ec8ef67597add4aa13271a92c7
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-19 17:25:05 +0800

    Def SpellCheck

--- SpellingChecker.py
+++ SpellingChecker.py
@@ -7,6 +7,14 @@ root.title("Spelling Checker")
 root.geometry("700x400")
 root.config(background="#E6E6FA")
 
+def spellCheck():
+    word = text.get()
+    txt = TextBlob(word)
+    CorrectText = str(txt.CorractText())
+
+    RightText = Label(root, text = "Check Spelling", font=("Helvetica", 30, "bold"), bg="#CDC1C5", fg="black")
+    RightText.place(x=100, y=250)
+    spell.config(text=CorrectText)
 
 heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), fg="#333333") 
 heading.pack (pady=(50,0))
@@ -15,7 +23,7 @@ text = Entry(root, justify="center", width = 15, font=("poppins", 25), bg="#FFF0
 text.pack(pady=10)
 text.focus()
 
-button = Button(root, text="CHECK", width=15, font=("arial", 20, "bold"), fg="#FAEBD7", bg="#FFEFDB")
+button = Button(root, text="CHECK", width=15, font=("arial", 20, "bold"), fg="#FAEBD7", bg="#8B8386")
 button.pack()
 
 
