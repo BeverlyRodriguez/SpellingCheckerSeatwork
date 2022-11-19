beac14d002f50ceee7c2d584af2fc3994b4d4d75
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-19 17:03:15 +0800

    Entering a Text Design 2

--- SpellingChecker.py
+++ SpellingChecker.py
@@ -9,5 +9,9 @@ root.config(background="black")
 
 
 heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), bg="#white", fg="#gray") 
+heading.pack (pady=(50,0))
 
+text = Entry(root, justify="center", width = 15, font=("poppins", 25), bg="blue", border=2)
+text.pack(pady=10)
+text.focus()
 root.mainloop()
