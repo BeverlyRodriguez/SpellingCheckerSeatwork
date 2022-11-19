3c02506eb0fac4f79f989da7b77f157179529c24
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-19 18:47:26 +0800

    Adding border to correct answer

--- SpellingChecker.py
+++ SpellingChecker.py
@@ -16,7 +16,7 @@ def spellCheck():
     CorrectText.place(x=80, y=250)
     spell.config(text=RightText)
 
-heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), fg="#E6E6FA") 
+heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), bg="#C1C1FF", fg="#2F2F4F") 
 heading.pack (pady=(50,0))
 
 text = Entry(root, justify="center", width = 15, font=("poppins", 25), bg="#FFF0F5", border=3)
@@ -26,7 +26,7 @@ text.focus()
 button = Button(root, text="CHECK", width=15, font=("arial", 20, "bold"), fg="#FAEBD7", bg="#000000", command=spellCheck)
 button.pack()
 
-spell = Label(root, font=("poppins", 20), bg="#C1C1FF", fg="#2F2F4F")
+spell = Label(root, font=("poppins", 20), bg="#FFF0F5", fg="#2F2F4F", border=2)
 spell.place(x=400, y=250)
 
 root.mainloop()
