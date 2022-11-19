075c97f831998ef8527928004cd047eeb236ea0a
Beverly Ann Rodriguez <rodriguezbeverlyann758@gmail.com>
2022-11-19 18:42:22 +0800

    Changing Border and colors

--- SpellingChecker.py
+++ SpellingChecker.py
@@ -5,28 +5,28 @@ from textblob import TextBlob
 root = Tk()
 root.title("Spelling Checker")
 root.geometry("700x400")
-root.config(background="#7979CD")
+root.config(background="#C1C1FF")
 
 def spellCheck():
     word = text.get()
     txt = TextBlob(word)
     RightText = str(txt.correct())
 
-    CorrectText = Label(root, text = "Correct Spelling: ", font=("poppins", 20, "bold"), bg="#C1C1FF", fg="#2F2F4F")
+    CorrectText = Label(root, text = "Correct Spelling: ", font=("poppins", 20, "bold"), bg="#C1C1FF", fg="#000000")
     CorrectText.place(x=80, y=250)
     spell.config(text=RightText)
 
-heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), bg="#6E7B8B", fg="#E6E6FA") 
+heading =Label(root, text ="Check Spelling", font=("Helvetica", 30, "bold"), fg="#E6E6FA") 
 heading.pack (pady=(50,0))
 
-text = Entry(root, justify="center", width = 15, font=("poppins", 25), bg="#FFF0F5", border=2)
+text = Entry(root, justify="center", width = 15, font=("poppins", 25), bg="#FFF0F5", border=3)
 text.pack(pady=10)
 text.focus()
 
-button = Button(root, text="CHECK", width=15, font=("arial", 20, "bold"), fg="#FAEBD7", bg="#8B8386", command=spellCheck)
+button = Button(root, text="CHECK", width=15, font=("arial", 20, "bold"), fg="#FAEBD7", bg="#000000", command=spellCheck)
 button.pack()
 
-spell = Label(root, font=("poppins", 20), bg="#333333", fg="black")
-spell.place(x=450, y=250)
+spell = Label(root, font=("poppins", 20), bg="#C1C1FF", fg="#2F2F4F")
+spell.place(x=400, y=250)
 
 root.mainloop()
